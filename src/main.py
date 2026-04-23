2a78a1d31fbbfb5d5cc9712c27359f2052ed688c
blastrial <erdelleyla3@gmail.com>
2026-04-23 22:03:55 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-from src.config import settings
-
-app = FastAPI(title=settings.app_name, version=settings.version)
-
-
-@app.get("/")
-def home():
-    return {
-        "message": "NewsFeed API is running",
-        "app": settings.app_name,
-        "version": settings.version,
-    }
+from src.app import app
