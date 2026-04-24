96b2c52d2b8fa00b2b1494f5904606a80dd4151c
blastrial <erdelleyla3@gmail.com>
2026-04-24 13:34:17 +0300

    Update services.py

--- src/services.py
+++ src/services.py
@@ -1,4 +1,4 @@
-from src.config import DEFAULT_CATEGORY
+from src.config import DEFAULT_CATEGORY, DEFAULT_LANGUAGE
 from src.parsers import parse_feed_items
 from src.utils import normalize_text, shorten_text, make_slug
 
@@ -36,4 +36,12 @@ def get_stats():
     return {
         "total_sources": len(get_sources()),
         "total_articles": len(get_articles()),
+    }
+
+
+def get_settings():
+    return {
+        "category": DEFAULT_CATEGORY,
+        "limit": 10,
+        "language": DEFAULT_LANGUAGE,
     }
